5794739c3cc7b816c636546d567388ef0bca6886
Andrei Papruha <andreipopruha@gmail.com>
2023-01-22 23:51:46 +0400

    first commit

--- src/test/java/helpers/TestBase.java
+++ src/test/java/helpers/TestBase.java
@@ -8,21 +8,34 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.util.Map;
 
 public class TestBase {
-
+    String asd = System.getProperty("browserName","chrome");
     @BeforeAll
-    static void beforeAll() {
+    static void beforeAll() throws MalformedURLException {
         new ChromeOptions().addArguments("start-maximized");
         Configuration.baseUrl = System.getProperty("base_url","https://demoqa.com");
-        Configuration.browser = System.getProperty("browser","chrome");
+        Configuration.browser = System.getProperty("browserName","chrome");
         Configuration.remote = System.getProperty("remoteUrl","https://user1:1234@selenoid.autotests.cloud/wd/hub");
         Configuration.browserSize = System.getProperty("browserSize","1320x800");
         Configuration.browserVersion = System.getProperty("browserVersion","100");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
+        capabilities.setCapability("browserName", Configuration.browser);
+        capabilities.setCapability("browserVersion", Configuration.browserVersion);
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        RemoteWebDriver driver = new RemoteWebDriver(
+                URI.create(Configuration.remote).toURL(),
+                capabilities
+        );
         Configuration.browserCapabilities = capabilities;
     }
 
