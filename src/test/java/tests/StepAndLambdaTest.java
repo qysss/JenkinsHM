c5a71fe693220f67b26456d65844709878dfe359
Andrei Papruha <andreipopruha@gmail.com>
2023-01-22 13:27:00 +0400

    first commit

--- src/test/java/tests/StepAndLambdaTest.java
+++ src/test/java/tests/StepAndLambdaTest.java
@@ -1,13 +1,8 @@
 package tests;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.TestBase;
 import helpers.WebSteps;
-import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selectors.withText;
@@ -21,19 +16,6 @@ public class StepAndLambdaTest extends TestBase {
     WebSteps steps = new WebSteps();
     private static final String REPOSITORY = "qysss/AllureHM";
     private static final int ISSUE = 1;
-
-    @BeforeAll
-    public static void setUp() {
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-        Configuration.timeout = 10000;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-    }
-
-    @BeforeEach
-    public void addListener() {
-        SelenideLogger.addListener("allure", new AllureSelenide());
-    }
 
     @Test
     public void testLamdaStep() {
