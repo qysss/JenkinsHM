756646417afa4cb191ce80690986d9427de81ff0
Andrei Papruha <andreipopruha@gmail.com>
2023-01-22 23:28:14 +0400

    first commit

--- src/test/java/helpers/TestBase.java
+++ src/test/java/helpers/TestBase.java
@@ -6,12 +6,14 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
+        new ChromeOptions().addArguments("start-maximized");
         Configuration.baseUrl = System.getProperty("base_url","https://demoqa.com");
         Configuration.browser = System.getProperty("browser","chrome");
         Configuration.remote = System.getProperty("remoteUrl","https://user1:1234@selenoid.autotests.cloud/wd/hub");
